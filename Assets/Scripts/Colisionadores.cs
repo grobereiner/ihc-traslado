a5bb55276129323fe071db4a351a7b437bfaeb37
grobereiner <grover.ugarte@utec.edu.pe>
2023-09-09 11:47:43 -0500

    .

---
+++ Assets/Scripts/Colisionadores.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colisionadores : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject general = GameObject.Find("zimCreateArchive_demo_export");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
